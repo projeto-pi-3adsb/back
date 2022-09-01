d55ae74798a42efaa35c6475e14771d2cfa16176
samuckqadev <samuel.tomimatu@bandtec.com.br>
2022-09-01 11:41:54 -0300

    Stucture pattern

---
+++ hemo-app/src/main/java/br/com/hemomanager/hemoapp/HemoAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.hemomanager.hemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HemoAppApplication.class, args);
+	}
+
+}
---
+++ hemo-app/src/test/java/br/com/hemomanager/hemoapp/HemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hemomanager.hemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
