999e58ab1f85659d754dee818dd8e8b26b264bd5
CindyKanashiro <cindy.goncalves@bandtec.com.br>
2022-10-19 14:00:29 -0300

    Arquivo csv feito

--- hemomanager/src/main/java/com/example/start/hemomanager/controller/UserController.java
+++ hemomanager/src/main/java/com/example/start/hemomanager/controller/UserController.java
@@ -1,15 +1,25 @@
 package com.example.start.hemomanager.controller;
 
+import com.example.start.hemomanager.listaObj.ListaObj;
 import com.example.start.hemomanager.service.LoginUserService;
 import com.example.start.hemomanager.service.SignUserService;
+import com.example.start.hemomanager.shared.enumerators.BloodType;
 import com.example.start.hemomanager.shared.user.User;
 import com.example.start.hemomanager.shared.user.donor.Donor;
 import com.example.start.hemomanager.shared.user.hemocenter.officer.Manager;
 import com.example.start.hemomanager.shared.user.hemocenter.officer.Nurse;
 import com.example.start.hemomanager.shared.user.hemocenter.officer.Officer;
 import com.example.start.hemomanager.shared.user.hemocenter.officer.Receptionist;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+
+import java.io.*;
+import java.util.*;
 
 @RestController
 @RequestMapping("/users")
@@ -71,8 +81,67 @@ public class UserController {
         return this.signUserService.getAllAuthorizedStockOfficers();
     }
 
-//    @GetMapping("/csv")
-//    public List<Donor> donorsDowloads(){
-//
-//    }
+    @GetMapping("/csv")
+    public ResponseEntity<Resource> donorsDowloads() throws FileNotFoundException {
+        FileWriter arq = null; // objeto que representa o arquivo de gravação
+        Formatter saida = null; // objeto usado para gravar no arquivo
+        Boolean deuRuim = false;
+        String nomeArq = "Donors.csv"; // acrescenta a extensão .csv ao nome do aquivo
+        ListaObj<Donor> donors = new ListaObj<>(10);
+        Donor donorTest = new Donor(UUID.randomUUID(),"Lala","23774360804","cindy@gmail.com",
+                "123456789",true, new Date(),"F", BloodType.ABNEG,"12991340567"
+        ,true,"01313020","175");
+
+
+        donors.add(donorTest);
+
+        //bloco para abrir o arquivo
+        try{
+            arq = new FileWriter(nomeArq); // cria ou abre o arquivo
+            saida = new Formatter(arq);
+
+        } catch (IOException erro){
+            System.out.println("Erro ao abrir o aquivo");
+            System.exit(1);
+        }
+
+        //bloco para gravar o arquivo
+        try{
+            // saida.format("ID;NOME;VALOR;ESTOQUE\n");
+
+            for (int i = 0; i < donors.getTamanho();i++){
+                Donor donor = donors.getElemento(i);
+                saida.format("%s;%s;%s\n",donor.getName(),donor.getCpfCnpj(),
+                        donor.getEmail());
+            }
+        } catch (FormatterClosedException erro){
+            System.out.println("Erro ao gravar o aquivo");
+            deuRuim = true;
+        }
+        finally {
+            saida.close();
+
+            try{
+                arq.close();
+
+            }catch (IOException erro){
+                System.out.println("Erro ao fechar o aquivo");
+                deuRuim = true;
+            }
+            if (deuRuim){
+                System.exit(1);
+            }
+        }
+
+        File initialFile = new File(nomeArq);
+        InputStream targetStream = new FileInputStream(initialFile);
+
+        InputStreamResource file = new InputStreamResource(targetStream);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + nomeArq)
+                .contentType(MediaType.parseMediaType("application/csv"))
+                .body(file);
+
+    }
 }
