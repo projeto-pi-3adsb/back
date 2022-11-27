4971bfd071198d6a0be0c4a935aac8501ec83f67
Adalberto <nascimentoadalberto678@gmail.com>
2022-11-27 14:44:04 -0300

    adicionando validação dos dados de entrada dos DTOs

---
+++ src/main/java/com/example/start/hemomanager/v2/dto/InputValidation.java
@@ -0,0 +1,29 @@
+package com.example.start.hemomanager.v2.dto;
+
+public class InputValidation {
+    public boolean validateDonorSex(String sex) {
+        switch (sex) {
+            case "MALE":
+            case "FEMALE":
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean validateBloodType(String bloodType) {
+        switch (bloodType) {
+            case "A+":
+            case "A-":
+            case "B+":
+            case "B-":
+            case "AB+":
+            case "AB-":
+            case "O+":
+            case "O-":
+                return true;
+            default:
+                return false;
+        }
+    }
+}
