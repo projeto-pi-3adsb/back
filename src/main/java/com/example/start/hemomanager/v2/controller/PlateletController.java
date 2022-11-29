81d01c9b22459e0b1ff321d1c63db3d6757f5c75
CindyKanashiro <cindy.goncalves@bandtec.com.br>
2022-11-29 02:59:46 -0300

    Upload do arquivo txt e csv

--- src/main/java/com/example/start/hemomanager/v2/controller/PlateletController.java
+++ src/main/java/com/example/start/hemomanager/v2/controller/PlateletController.java
@@ -2,22 +2,22 @@ package com.example.start.hemomanager.v2.controller;
 
 import com.example.start.hemomanager.v2.domain.Donor;
 import com.example.start.hemomanager.v2.repository.DonorRepository;
-import com.example.start.hemomanager.v2.utils.ListaObj;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.nio.file.*;
 import java.time.LocalDate;
-import java.util.Formatter;
-import java.util.FormatterClosedException;
-import java.util.Optional;
+import java.util.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 @RestController
 @RequestMapping("/platelets")
@@ -25,6 +25,8 @@ public class PlateletController {
 
     @Autowired
     private DonorRepository donorRepository;
+
+    private final Path root = Paths.get("uploads");
 
     @GetMapping("/dowload-csv")
     public ResponseEntity<Resource> dowloadsCsv(Integer donorId) throws FileNotFoundException {
@@ -129,5 +131,80 @@ public class PlateletController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + nomeArq)
                 .contentType(MediaType.parseMediaType("application/txt"))
                 .body(file);
+    }
+
+    @PostMapping("/upload-csv")
+    public String uploadCsv(@RequestParam("file") MultipartFile file) throws IOException {
+        FileReader arq = null;
+        Scanner entrada = null;
+
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(Objects.requireNonNull(file.getOriginalFilename())));
+
+            arq = new FileReader(file.getOriginalFilename());
+            entrada = new Scanner(arq).useDelimiter(";|\\n");
+
+            while(entrada.hasNext()){
+                Donor donor = new Donor(
+                    entrada.next(),
+                    entrada.next(),
+                    "123",
+                    entrada.next(),
+                    LocalDate.parse(entrada.next()),
+                    entrada.next(),
+                    entrada.next(),
+                    true
+                );
+
+                donorRepository.save(donor);
+            }
+
+            Files.delete(this.root.resolve(Objects.requireNonNull(file.getOriginalFilename())));
+        } catch (Exception e) {
+            Files.delete(this.root.resolve(Objects.requireNonNull(file.getOriginalFilename())));
+
+            if (e instanceof FileAlreadyExistsException) {
+                throw new RuntimeException("A file of that name already exists.");
+            }
+
+            return e.getMessage();
+        }
+        return "Sucesso";
+    }
+
+    @PostMapping("/upload-txt")
+    public String uploadTxt(@RequestParam("file") MultipartFile file) throws IOException {
+        FileReader arq = null;
+        Scanner entrada = null;
+
+        try {
+            Files.copy(file.getInputStream(), this.root.resolve(Objects.requireNonNull(file.getOriginalFilename())));
+
+            arq = new FileReader(file.getOriginalFilename());
+            BufferedReader lerArq = new BufferedReader(arq);
+
+            Donor donor = new Donor(
+                    lerArq.readLine(),
+                    lerArq.readLine(),
+                    "123",
+                    lerArq.readLine(),
+                    LocalDate.parse(lerArq.readLine()),
+                    lerArq.readLine(),
+                    lerArq.readLine(),
+                    true
+            );
+            donorRepository.save(donor);
+
+            Files.delete(this.root.resolve(Objects.requireNonNull(file.getOriginalFilename())));
+        } catch (Exception e) {
+            Files.delete(this.root.resolve(Objects.requireNonNull(file.getOriginalFilename())));
+
+            if (e instanceof FileAlreadyExistsException) {
+                throw new RuntimeException("A file of that name already exists.");
+            }
+
+            return e.getMessage();
+        }
+        return "Sucesso";
     }
 }
