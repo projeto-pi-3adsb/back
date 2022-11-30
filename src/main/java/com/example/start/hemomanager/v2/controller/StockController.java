16b145541d866bc1b954e143745918d3dad0003f
CindyKanashiro <cindy.goncalves@bandtec.com.br>
2022-11-29 21:29:28 -0300

    Pilha feita com Hard code

--- src/main/java/com/example/start/hemomanager/v2/controller/StockController.java
+++ src/main/java/com/example/start/hemomanager/v2/controller/StockController.java
@@ -10,11 +10,15 @@ import com.example.start.hemomanager.v2.response.StockFullResponse;
 import com.example.start.hemomanager.v2.response.StockSimpleResponse;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 @RestController @RequestMapping("/stock")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -64,5 +68,48 @@ public class StockController {
     @GetMapping("/full/{hemocenter}")
     public ResponseEntity<List<StockFullResponse>> getAllFromStockHemocenter(@PathVariable Integer hemocenter) {
         return (hemocenterRepository.existsById(hemocenter)) ? ResponseEntity.status(200).body(stockRepository.findByHemocenter(hemocenter)) : ResponseEntity.status(404).build();
+    }
+
+    @GetMapping("/download-csv/{hemocenter}")
+    public ResponseEntity<Resource> downloadCSV(@PathVariable Integer hemocenter) throws FileNotFoundException {
+        List<StockFullResponse> stockList = stockRepository.findByHemocenter(hemocenter);
+        String nomeArq = "stock.csv";
+
+        try {
+            FileWriter arq = new FileWriter(nomeArq);
+            Formatter saida = this.funcaoDoInferno(stockList, new Formatter(arq));
+            saida.close();
+            arq.close();
+        } catch (IOException erro) {
+            System.out.println("Erro ao abrir o aquivo");
+            System.exit(1);
+        } catch (FormatterClosedException erro) {
+            System.out.println("Erro ao gravar o aquivo");
+        }
+
+        File initialFile = new File(nomeArq);
+        InputStream targetStream = new FileInputStream(initialFile);
+
+        InputStreamResource file = new InputStreamResource(targetStream);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + nomeArq)
+                .contentType(MediaType.parseMediaType("application/csv"))
+                .body(file);
+    }
+
+    private Formatter funcaoDoInferno(List<StockFullResponse> stockList, Formatter saida) {
+        if (stockList.size() > 1) {
+            funcaoDoInferno(stockList.subList(0, stockList.size()-1), saida);
+        }
+
+        StockFullResponse tockStock = stockList.get(stockList.size()-1);
+        saida.format("%s;%s;%s;%s;\n",
+            tockStock.getBloodType(),
+            tockStock.getCollectionDate(),
+            tockStock.getInsertDate()
+        );
+
+        return saida;
     }
 }
